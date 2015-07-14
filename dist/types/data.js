a92514165ac2ab9d77efdf52e416b342dbeec10b
pemrouz <p3pedram@hotmail.com>
2015-07-14 02:25:13 +0100

    update builds

--- dist/types/data.js
+++ dist/types/data.js
@@ -29,6 +29,7 @@
 /* istanbul ignore next */
 /* istanbul ignore next */
 /* istanbul ignore next */
+/* istanbul ignore next */
 var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
 
 module.exports = {
