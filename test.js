ceafe2d2fefff5c78368e7c17c85ff6d9ef75ef6
pemrouz <p3pedram@hotmail.com>
2018-03-18 02:56:47 +0000

    chore: convert tests to tap

--- test.js
+++ test.js
@@ -1,228 +1,233 @@
-var header = require('utilise/header')
-  , expect = require('chai').expect
-  , is = require('utilise/is')
-  , core = require('./')
+const { is, header }  = require('utilise/pure')
+    , { test } = require('tap')
+    , core = require('./')
 
-describe('Core', function() {
-  
-  it('should create empty resource from name', function(){  
-    var ripple = core()
-    expect(ripple('foo', 'bar')).to.eql('bar')
-  })
-
-  it('should fail to create resource it does not understand', function(){  
-    var ripple = core()
-    expect(ripple('foo')).to.be.not.ok
-  })
-
-  it('should set content-type by default', function(){  
-    var ripple = core()
-    ripple('foo', 'bar')
-    expect(header('content-type')(ripple.resources['foo'])).to.be.equal('text/plain')
-  })
-
-  it('should create resource using alias', function(){  
-    var ripple = core()
-    expect(ripple.register('foo', 'bar')).to.eql('bar')
-  })
-
-  it('should create and get resource from name', function(){  
-    var ripple = core()
-    ripple('foo', 'bar')
-    expect(ripple('foo')).to.eql('bar')
-  })
-
-  it('should create and get resource from obj', function(){  
-    var ripple = core()
-    expect(ripple({ name: 'foo', body: 'bar', headers: {} })).to.eql('bar')
-    expect(ripple.resources.foo).to.eql({
-      name: 'foo'
-    , body: 'bar'
-    , headers: { 'content-type': 'text/plain' } 
-    })
-  })
-
-  it('should import multiple resources from object', function(){  
-    var ripple = core()
-    expect(ripple({ 
-      foo: { name: 'foo', body: 'foo', headers: {} }
-    , bar: { name: 'bar', body: 'bar', headers: {} }
-    })).to.eql([ 'foo', 'bar' ])
-
-    expect(ripple.resources).to.eql({ 
-      foo: { name: 'foo', body: 'foo', headers: { 'content-type': 'text/plain' } }
-    , bar: { name: 'bar', body: 'bar', headers: { 'content-type': 'text/plain' } }
-    })
-  })
-  it('should create import resources from another node', function(){  
-    var ripple = core()
-    ripple({ name: 'foo', body: 'bar', headers: {} })
-    var ripple2 = core()
-    ripple2(ripple)
-    expect(ripple2.resources.foo).to.eql({
-      name: 'foo'
-    , body: 'bar'
-    , headers: { 'content-type': 'text/plain' } 
-    })
-  })
-
-  it('should use explicitly set headers', function(){  
-    var ripple = core()
-    expect(ripple({
-      name: 'sth'
-    , body: { a : 1 }
-    , headers: { 'content-type': 'text/plain' } 
-    })).to.eql({ a : 1 })
-    expect(ripple.resources.sth).to.eql({
-      name: 'sth'
-    , body: { a : 1 }
-    , headers: { 'content-type': 'text/plain' } 
-    })
-  })
-
-  it('should support method chaining api', function(){  
-    var ripple = core()
-    ripple
-      .resource('foo', 'bar')
-      .resource('bar', 'foo')
-
-    expect(ripple('foo')).to.eql('bar')
-    expect(ripple('bar')).to.eql('foo')
-  })
-
-  it('should create two different ripple nodes', function(){
-    var ripple1 = core()
-      , ripple2 = core()
-    
-    expect(ripple1).to.not.equal(ripple2)
-  })
-
-  it('should register multiple resources', function(){
-    var ripple = core()
-    ripple([{name:'foo', body:'1'}, {name:'bar', body:'1'}])
-    
-    expect('foo' in ripple.resources).to.be.ok
-    expect('bar' in ripple.resources).to.be.ok
-  })
-
-  it('should destroy existing headers by default', function(){
-    var ripple = core()
-    ripple({ name: 'name', body: 'foo', headers: { 'foo': 'bar' }})
-    expect(ripple.resources.name.headers.foo).to.be.eql('bar')
-
-    ripple({ name:'name', body: 'baz' })
-    expect(ripple.resources.name.headers.foo).to.be.not.ok
-  })
-
-  it('should fail if cannot interpret resource', function(){
-    var ripple = core()
-    expect(ripple('foo', [])).to.not.be.ok
-    expect(ripple('bar')).to.not.be.ok
-
-    expect('foo' in ripple.resources).to.be.false
-    expect('bar' in ripple.resources).to.be.false
-  })
-
-  it('should fail if uses api incorrectly', function(){
-    var ripple = core()
-    expect(ripple()).to.be.eql(ripple)
-    expect(ripple(String)).to.not.be.ok
-    expect(ripple.resources).to.eql({})
-  })
-
-  it('should skip empty objects', function(){
-    var ripple = core()
-    expect(ripple({})).to.be.eql([])
-    expect(ripple.resources).to.eql({})
-  })
-
-  it('should not register type it does not understand', function(){
-    var ripple = core()
-    expect(ripple({ name: 'foo', body: 'foo', headers: { 'content-type': 'application/jsx' }})).to.not.be.ok
-    expect(ripple.resources).to.eql({})
-  })
-
-  it('should emit global change events', function(){
-    var ripple = core()
-      , params
-      , fn = function(name, change){ params = [name, change] }
-
-    ripple.on('change', fn)
-    ripple('foo', 'bar')
-    expect(params).to.eql(['foo', { type: 'update', value: 'bar', time: undefined }])
-
-    ripple.types['data'] = { header: 'data', check: String }
-    ripple('boo', { log: ['baz'] })
-    expect(params).to.eql(['boo', { type: 'update', value: { log: ['baz'] }, time: 0 }])
-  })
-
-  it('should indicate if new resource', function(done){
-    var ripple = core()
-    ripple.once('change', function(d, change){
-      expect(d).to.eql('foo')
-      expect(change).to.eql({ type: 'update', value: 'foo', time: undefined })
-      ripple.once('change', function(d, change){
-        expect(d).to.eql('foo')
-        expect(change).to.eql({ type: 'update', value: 'bar', time: undefined })
-        done()
-      })
-      ripple('foo', 'bar')
-    })
-    ripple('foo', 'foo')
-  })
-
-  it('should register new types by order', function(){
-    var ripple = core()
-    ripple('text', 'text')
-    expect(ripple.resources.text.headers['content-type']).to.eql('text/plain')
-
-    ripple.types['priority/low'] = {
-      header: 'priority/low'
-    , check: String
-    , priority: -10
-    }
-
-    ripple('low', 'low')
-    expect(ripple.resources.low.headers['content-type']).to.eql('text/plain')
-
-    ripple.types['priority/high'] = {
-      header: 'priority/high'
-    , check: String
-    , priority: 10
-    }
-
-    ripple('high', 'high')
-    expect(ripple.resources.high.headers['content-type']).to.eql('priority/high')
-  })  
-
-  it('should register promise - resource', function(done){
-    var ripple = core()
-    ripple(Promise.resolve({ name:'foo', body: 'bar' }))
-      .then(d => expect('foo' in ripple.resources).to.be.ok)
-      .then(d => expect(ripple.resources.foo.body).to.be.eql('bar'))
-      .then(d => done())
-
-    expect('foo' in ripple.resources).to.not.be.ok
-  })
-
-  it('should register promise - body 1', function(done){
-    var ripple = core()
-    ripple('foo', Promise.resolve('bar'))
-      .then(d => expect('foo' in ripple.resources).to.be.ok)
-      .then(d => expect(ripple.resources.foo.body).to.be.eql('bar'))
-      .then(d => done())
-
-    expect('foo' in ripple.resources).to.not.be.ok
-  })
-
-  it('should register promise - body 2', function(done){
-    var ripple = core()
-    ripple({ name: 'foo', body: Promise.resolve('bar') })
-      .then(d => expect('foo' in ripple.resources).to.be.ok)
-      .then(d => expect(ripple.resources.foo.body).to.be.eql('bar'))
-      .then(d => done())
-
-    expect('foo' in ripple.resources).to.not.be.ok
-  })
-
+test('should create empty resource from name', ({ plan, same }) => {  
+  plan(1)
+  const ripple = core()
+  same(ripple('foo', 'bar'), 'bar')
 })
+
+test('should fail to create resource it does not understand', ({ plan, same }) => {  
+  plan(1)
+  const ripple = core()
+  same(ripple('foo'), undefined)
+})
+
+test('should set content-type by default', ({ plan, same }) => {  
+  plan(1)
+  const ripple = core()
+  ripple('foo', 'bar')
+  same(header('content-type')(ripple.resources['foo']), 'text/plain')
+})
+
+test('should create resource using alias', ({ plan, same }) => {  
+  plan(1)
+  const ripple = core()
+  same(ripple.register('foo', 'bar'), 'bar')
+})
+
+test('should create and get resource from name', ({ plan, same }) => {  
+  plan(1)
+  const ripple = core()
+  ripple('foo', 'bar')
+  same(ripple('foo'), 'bar')
+})
+
+test('should create and get resource from obj', ({ plan, same }) => {  
+  plan(2)
+  const ripple = core()
+  same(ripple({ name: 'foo', body: 'bar', headers: {} }), 'bar')
+  same(ripple.resources.foo, {
+    name: 'foo'
+  , body: 'bar'
+  , headers: { 'content-type': 'text/plain' } 
+  })
+})
+
+test('should import multiple resources from object', ({ plan, same }) => {  
+  plan(2)
+  const ripple = core()
+  same(ripple({ 
+    foo: { name: 'foo', body: 'foo', headers: {} }
+  , bar: { name: 'bar', body: 'bar', headers: {} }
+  }), [ 'foo', 'bar' ])
+
+  same(ripple.resources, { 
+    foo: { name: 'foo', body: 'foo', headers: { 'content-type': 'text/plain' } }
+  , bar: { name: 'bar', body: 'bar', headers: { 'content-type': 'text/plain' } }
+  })
+})
+
+test('should create import resources from another node', ({ plan, same }) => {  
+  plan(1)
+  const ripple = core()
+  ripple({ name: 'foo', body: 'bar', headers: {} })
+  const ripple2 = core()
+  ripple2(ripple)
+  same(ripple2.resources.foo, {
+    name: 'foo'
+  , body: 'bar'
+  , headers: { 'content-type': 'text/plain' } 
+  })
+})
+
+test('should use explicitly set headers', ({ plan, same }) => {  
+  plan(2)
+  const ripple = core()
+  same(ripple({
+    name: 'sth'
+  , body: { a : 1 }
+  , headers: { 'content-type': 'text/plain' } 
+  }), { a : 1 })
+  same(ripple.resources.sth, {
+    name: 'sth'
+  , body: { a : 1 }
+  , headers: { 'content-type': 'text/plain' } 
+  })
+})
+
+test('should create two different ripple nodes', ({ plan, ok }) => {
+  plan(1)
+  const ripple1 = core()
+      , ripple2 = core()
+  
+  ok(ripple1 !== ripple2)
+})
+
+test('should register multiple resources', ({ plan, ok }) => {
+  plan(2)
+  const ripple = core()
+  ripple([{name:'foo', body:'1'}, {name:'bar', body:'1'}])
+  
+  ok('foo' in ripple.resources)
+  ok('bar' in ripple.resources)
+})
+
+test('should destroy existing headers by default', ({ plan, same }) => {
+  plan(2)
+  const ripple = core()
+  ripple({ name: 'name', body: 'foo', headers: { 'foo': 'bar' }})
+  same(ripple.resources.name.headers.foo, 'bar')
+
+  ripple({ name:'name', body: 'baz' })
+  same(ripple.resources.name.headers.foo, undefined)
+})
+
+test('should fail if cannot interpret resource', ({ plan, notOk }) => {
+  plan(4)
+  const ripple = core()
+  notOk(ripple('foo', []))
+  notOk(ripple('bar'))
+
+  notOk('foo' in ripple.resources)
+  notOk('bar' in ripple.resources)
+})
+
+test('should fail if uses api incorrectly', ({ plan, same }) => {
+  plan(3)
+  const ripple = core()
+  same(ripple(), ripple)
+  same(ripple(String), false)
+  same(ripple.resources, {})
+})
+
+test('should skip empty objects', ({ plan, same }) => {
+  plan(2)
+  const ripple = core()
+  same(ripple({}), [])
+  same(ripple.resources, {})
+})
+
+test('should not register type it does not understand', ({ plan, same }) => {
+  plan(2)
+  const ripple = core()
+  same(ripple({ name: 'foo', body: 'foo', headers: { 'content-type': 'application/jsx' }}), false)
+  same(ripple.resources, {})
+})
+
+test('should emit global change events', ({ plan, same }) => {
+  plan(2)
+  let ripple = core()
+    , params
+    , fn = (name, change) => { params = [name, change] }
+
+  ripple.on('change', fn)
+  ripple('foo', 'bar')
+  same(params, ['foo', { type: 'update', value: 'bar', time: undefined }])
+
+  ripple.types['data'] = { header: 'data', check: String }
+  ripple('boo', { log: ['baz'] })
+  same(params, ['boo', { type: 'update', value: { log: ['baz'] }, time: 0 }])
+})
+
+test('should indicate if new resource', ({ plan, same }) => {
+  plan(4)
+  const ripple = core()
+  ripple.once('change', (d, change) => {
+    same(d, 'foo')
+    same(change, { type: 'update', value: 'foo', time: undefined })
+    ripple.once('change', (d, change) => {
+      same(d, 'foo')
+      same(change, { type: 'update', value: 'bar', time: undefined })
+    })
+    ripple('foo', 'bar')
+  })
+  ripple('foo', 'foo')
+})
+
+test('should register new types by order', ({ plan, same }) => {
+  plan(3)
+  const ripple = core()
+  ripple('text', 'text')
+  same(ripple.resources.text.headers['content-type'], 'text/plain')
+
+  ripple.types['priority/low'] = {
+    header: 'priority/low'
+  , check: String
+  , priority: -10
+  }
+
+  ripple('low', 'low')
+  same(ripple.resources.low.headers['content-type'], 'text/plain')
+
+  ripple.types['priority/high'] = {
+    header: 'priority/high'
+  , check: String
+  , priority: 10
+  }
+
+  ripple('high', 'high')
+  same(ripple.resources.high.headers['content-type'], 'priority/high')
+})  
+
+test('should register promise - resource', ({ plan, same}) => {
+  plan(3)
+  const ripple = core()
+  ripple(Promise.resolve({ name:'foo', body: 'bar' }))
+    .then(d => same('foo' in ripple.resources, true))
+    .then(d => same(ripple.resources.foo.body, 'bar'))
+    
+  same('foo' in ripple.resources, false)
+})
+
+test('should register promise - body 1', ({ plan, same }) => {
+  plan(3)
+  const ripple = core()
+  ripple('foo', Promise.resolve('bar'))
+    .then(d => same('foo' in ripple.resources, true))
+    .then(d => same(ripple.resources.foo.body, 'bar'))
+    
+  same('foo' in ripple.resources, false)
+})
+
+test('should register promise - body 2', ({ plan, same }) => {
+  plan(3)
+  const ripple = core()
+  ripple({ name: 'foo', body: Promise.resolve('bar') })
+    .then(d => same('foo' in ripple.resources, true))
+    .then(d => same(ripple.resources.foo.body, 'bar'))
+    
+  same('foo' in ripple.resources, false)
+})
+
